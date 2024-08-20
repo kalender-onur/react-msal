3ed04d9ffc4936ad2940671b0efbc37a6a5f57e1
Onur Kalender <KOKALENDER@fnss.com.tr>
2024-08-20 16:25:33 +0300

    modified

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { EventType, PublicClientApplication } from "@azure/msal-browser";
+import { PublicClientApplication } from "@azure/msal-browser";
 import App from "./App";
 import { msalConfig } from "./configs/auth-config";
 import { MsalProvider } from "@azure/msal-react";
 
 const msalInstance = new PublicClientApplication(msalConfig);
-
 createRoot(document.getElementById("root")!).render(
   <MsalProvider instance={msalInstance}>
     <App />
